6f5dae52335ad08b1e50328c23960aad173b432e
Xiao Xiao <Yvonexiaoxiao@gmail.com>
2014-02-09 16:46:21 -0800

    Create Terminals.java

---
+++ Terminals.java
@@ -0,0 +1,46 @@
+/**
+ * This class lists terminals used by the
+ * grammar specified in "cool.grammar".
+ */
+public class Terminals {
+	static public final short EOF = 0;
+	static public final short LPAREN = 1;
+	static public final short ID = 2;
+	static public final short LBRACE = 3;
+	static public final short NULL = 4;
+	static public final short MINUS = 5;
+	static public final short NEW = 6;
+	static public final short THIS = 7;
+	static public final short SUPER = 8;
+	static public final short INTEGER = 9;
+	static public final short STRING = 10;
+	static public final short BOOLEAN = 11;
+	static public final short NOT = 12;
+	static public final short IF = 13;
+	static public final short WHILE = 14;
+	static public final short RPAREN = 15;
+	static public final short SEMI = 16;
+	static public final short RBRACE = 17;
+	static public final short TYPE = 18;
+	static public final short VAR = 19;
+	static public final short COLON = 20;
+	static public final short ASSIGN = 21;
+	static public final short NATIVE = 22;
+	static public final short DEF = 23;
+	static public final short COMMA = 24;
+	static public final short ARROW = 25;
+	static public final short TIMES = 26;
+	static public final short DIV = 27;
+	static public final short EQUALS = 28;
+	static public final short CLASS = 29;
+	static public final short OVERRIDE = 30;
+	static public final short CASE = 31;
+	static public final short DOT = 32;
+	static public final short PLUS = 33;
+	static public final short STUB = 34;
+	static public final short EXTENDS = 35;
+	static public final short MATCH = 36;
+	static public final short ELSE = 37;
+	static public final short LE = 38;
+	static public final short LT = 39;
+}
